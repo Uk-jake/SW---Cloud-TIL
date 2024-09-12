1dea7f4d524b0dbee976793a2a1bc1bcb2a27986
Uk-jake <mag0225@naver.com>
2024-09-13 08:57:50 +0900

    add Python Function Example 0912 afternoon

--- Python/first.py
+++ Python/first.py
@@ -1,41 +1,46 @@
-# 매개변수가 없는 함수
-def noArguments() -> None:
-    for _ in range(3):
-        print("noArguments() called")
+import datetime
+import functools
 
-# 파이썬에서도 매개변수에 자료형을 기재하주는 것을 권장.
-def argFunc(arg1: str) -> None:
-    print("argFunc() called with arguments", arg1)
+def deco(func):
+    def inner():
+        print("running inner()")
+        func()
+        print("finished running inner()")
+    return inner
 
-def callByValue(arg1: int) -> None:
-    print(f"callByValue() called with arguments {arg1}")
-    arg1 = 20
-    print(f"arg1 is now {arg1}")
+@deco
+def target():
+    print("running target()")
 
-def orderParameter(a:int, b:int) -> None:
-    print(f"a is {a}, b is {b}")
-    
-def tupleReturn() -> tuple:
-    return 100, 200
-    
-# 매개변수가 여러 개인 경우 1개로 대입해서 분할한 후 사용
-def personalInfo(name:str, age:int, gender:bool) -> None :
-    print(f"name : {name}")
-    print(f"age : {age}")
-    print(f"gender : {gender}")
+#target()
 
-personalInfo("Jake", 25, 1)
-personalInfo(*["Jack", 23, 1])
-personalInfo(*{"name":"Adam", "age": 34, "gender" : 1})
-personalInfo(**{"name":"Adam", "age": 34, "gender" : 1})
+def logRunTiem(func):
+    def inner(*args):
+        print("running inner()")
+        start = datetime.datetime.now()
+        result = func(*args)
+        end = datetime.datetime.now()
+        print("finished running inner()")
+        print("running time: ", end - start)
+        name = func.__name__
+        return result
+    return inner
 
-#orderParameter(b=10, a=20)
-#print(tupleReturn())
+@functools.lru_cache()
+@logRunTiem
+def RecursiveFibo(n):
+    if n < 2:
+        return n
+    else:
+        return RecursiveFibo(n-1) + RecursiveFibo(n-2)
 
-# 매개변수의 설명을 추가할 수 있음.
-def annotationFunction(score : 'int >= 0') -> None :
-    print(f"점수 : {score}")
+@logRunTiem
+def IterativeFibo(n):
+    a, b = 0, 1
+    while n > 0:
+        a, b = b, a+b
+        n -= 1
+    return a
 
-annotationFunction.__doc__ = "설명란 추가"
-    
-help(annotationFunction)
+print(IterativeFibo(40))
+print(RecursiveFibo(40))
