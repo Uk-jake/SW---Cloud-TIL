9e62d34aae3ea5c1937ae6b0db9cd8ff56afa9d8
Uk-jake <mag0225@naver.com>
2024-10-21 22:34:41 +0900

    Add TIL 241021

--- Week6/CQRSExample/src/main/java/com/example/cqrsexample/KafkaConfiguration.java
+++ Week6/CQRSExample/src/main/java/com/example/cqrsexample/KafkaConfiguration.java
@@ -3,7 +3,7 @@ package com.example.cqrsexample;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
+//import org.springframework.boot.web.servlet.filter.OrderedFormContentFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
--- Week6/CQRSExample/src/main/java/com/example/cqrsexample/WebConfig.java
+++ Week6/CQRSExample/src/main/java/com/example/cqrsexample/WebConfig.java
@@ -1,15 +1,15 @@
 package com.example.cqrsexample;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.beans.factory.annotation.Configurable;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configurable
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http:/localhost:3000");
-    }
+// @Configurable
+// public class WebConfig implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry){
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http:/localhost:3000");
+//     }
 
-}
+// }
